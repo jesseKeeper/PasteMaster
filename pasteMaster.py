2d31fe9a9a2f9fc67e458fc8a12b88a4c7e95403
jesseKeeper <jesse.b.doelman@gmail.com>
2022-12-21 13:15:20 +0100

    fix

--- pasteMaster.py
+++ pasteMaster.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route("/home")
+@app.route("/index")
 def home():
    return render_template('index.html')
 if __name__ == '__main__':
