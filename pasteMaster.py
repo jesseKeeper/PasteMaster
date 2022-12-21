5d2c2a6cf0001cd75dc2b0b9500cf787da8e783f
jesseKeeper <jesse.b.doelman@gmail.com>
2022-12-21 12:00:23 +0100

    links changed

--- pasteMaster.py
+++ pasteMaster.py
@@ -6,3 +6,7 @@ def home():
    return render_template('index.html')
 if __name__ == '__main__':
    app.run()
+
+@app.route('/start.html', methods=['GET'])
+def start():
+   return render_template('start.html')
