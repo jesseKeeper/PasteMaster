1e7d5af43be3fc4abc9cf0ce99d655a16552c19e
jesseKeeper <jesse.b.doelman@gmail.com>
2022-12-21 13:19:18 +0100

    ./start.html in browser

--- pasteMaster.py
+++ pasteMaster.py
@@ -2,13 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/start')
 def home():
    return render_template('index.html')
 if __name__ == '__main__':
    app.run()
 
-@app.route('/start', methods=['GET'])
+@app.route('/start.html', methods=['GET'])
 def index():
    return render_template('start.html')
 
