16116422ead75c880dd15d65dbf4fe80fe17fcac
jesseKeeper <jesse.b.doelman@gmail.com>
2022-12-21 13:16:36 +0100

    fix

--- pasteMaster.py
+++ pasteMaster.py
@@ -2,14 +2,12 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-@app.route("/home")
-@app.route("/index")
 def home():
    return render_template('index.html')
 if __name__ == '__main__':
    app.run()
 
-@app.route('/start/', methods=['GET'])
+@app.route('/start.html', methods=['GET'])
 def start():
    return render_template('start.html')
 
