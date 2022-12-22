012e93d5e5267e58015a8575844ea1baf122d9e4
jesseKeeper <jesse.b.doelman@gmail.com>
2022-12-22 11:28:58 +0100

    fix

--- pasteMaster.py
+++ pasteMaster.py
@@ -17,7 +17,7 @@ def index():
 
 @app.route('/run.html')
 def run_script():
-   file = open(r'./Kickstarter/calibration/calibrate.py', 'r').read()
+   file = open(r'./src/pythonScript/detect.py', 'r').read()
    return exec(file)
 
 @app.errorhandler(404)
