817f6d3a2464ac5614fbb89cebf8036ad99d940c
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-09 15:25:41 +0100

    start file

--- static/site.js
+++ static/site.js
@@ -9,6 +9,7 @@ let pcb_count = 0;
 function addOption (_text, _id) {
     let pcb_id = _id + '_' + pcb_count;
     pcb_count++;
+    pcb_options.push(pcb_id);
 
     jQuery (
         '<p class="pcb-option" style="margin-top: 0px; margin-bottom: 0px;">' +
