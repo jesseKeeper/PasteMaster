14aa98685209d62733af5fb748ddd192fdc3850b
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-09 13:16:20 +0100

    start.html

--- static/site.js
+++ static/site.js
@@ -6,11 +6,11 @@ jQuery(document).on('click', '.header', function () {
     window.location.replace("./index.html");
 });
 
-function addOption () {
+function addOption (text) {
     jQuery (
         '<p class="pcb-option">' +
             '<input type="checkbox" id="rec1" name="rec1" value="1"> ' +
-            '<label for="1"> I have a boat</label>' +
+            '<label for="1">' + text + '</label>' +
         '</p>' 
         ) .insertAfter(jQuery('#pcb-options'));
 }
