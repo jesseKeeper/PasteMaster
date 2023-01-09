d5d3e0ee90bd9624f7f159507454e6f45bfbc7fc
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-09 13:59:42 +0100

    start file

--- static/site.js
+++ static/site.js
@@ -2,15 +2,22 @@ function test() {
     return "test";
 }
 
-jQuery(document).on('click', '.header', function () {
-    window.location.replace("./index.html");
-});
-
-function addOption (text) {
+function addOption (text, id) {
     jQuery (
         '<p class="pcb-option" style="margin-top: 0px; margin-bottom: 0px;">' +
-            '<input type="checkbox" id="rec1" name="rec1" value="1"> ' +
+            '<input type="checkbox" id="' + id + '" name="' + id + '" value="' + id + '"> ' +
             '<label for="1">' + text + '</label>' +
         '</p>' 
         ) .insertAfter(jQuery('#pcb-options'));
 }
+
+jQuery(document).on('click', '.header', function () {
+    window.location.replace("./index.html");
+});
+
+jQuery( "#pcb-options" ).load(function() {
+    addOption ('test', 'line');
+    addOption ('test', 'line');
+    addOption ('test', 'line');
+    addOption ('test', 'line');
+});
