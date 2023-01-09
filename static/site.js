2e2ad9d25dce406c94c5d6d3c0c840fa5f9ceaec
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-09 15:22:24 +0100

    start file

--- static/site.js
+++ static/site.js
@@ -2,20 +2,32 @@ function test() {
     return "test";
 }
 
-function addOption (text, id) {
+
+let pcb_options = [];
+let pcb_count = 0;
+
+function addOption (_text, _id) {
+    let pcb_id = _id + '_' + pcb_count;
+    pcb_count++;
+
     jQuery (
         '<p class="pcb-option" style="margin-top: 0px; margin-bottom: 0px;">' +
-            '<input type="checkbox" id="' + id + '" name="' + id + '" value="' + id + '" checked> ' +
-            '<label for="1">' + text + '</label>' +
+            '<input type="checkbox" id="' + pcb_id + '" name="' + _id + '" value="' + pcb_id + '" checked> ' +
+            '<label for="1">' + _text + '</label>' +
         '</p>' 
-        ) .insertAfter(jQuery('#pcb-options'));
+    ) .insertAfter(jQuery('#pcb-options'));
 }
 
 jQuery(document).on('click', '.header', function () {
     window.location.replace("./index.html");
 });
 
-$(function() {
+function returnAllCheckedPoints () {
+    return pcb_options;
+}
+
+
+jQuery (function() {
     addOption ('test', 'line');
     addOption ('test', 'line');
     addOption ('test', 'line');
