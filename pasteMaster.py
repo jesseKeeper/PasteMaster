d85d90e6ffe931cff21123cb75ca215044539794
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-10 14:52:27 +0100

    start test

--- pasteMaster.py
+++ pasteMaster.py
@@ -4,23 +4,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/index.html')
-def home():
-   return render_template('index.html')
 if __name__ == '__main__':
    app.run()
 
-@app.route('/start.html', methods=['GET'])
+@app.route('/index.html')
+def home():
+   return render_template('index.html')
+
+@app.route('/start', methods=['GET'])
 def index():
-   file = open(r'./src/pythonScript/test.py', 'r').read()
-   return render_template('start.html'), exec(file)
+   # file = open(r'./src/pythonScript/test.py', 'r').read()
+   # return render_template('start.html'), exec(file)
+   return render_template('start.html')
 
 # @app.route('/start.html', methods=['GET'])
 # def index():
 #    return render_template('start.html')
 
 
-@app.route('/run.html')
+# @app.route('/run')
 def run_script():
    # return db naam / file naam --> return foto + array
    return render_template('page_not_found.html')
