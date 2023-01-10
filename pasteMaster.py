374718326ce2f746cc0738c21254948c980fe461
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-10 11:00:27 +0100

    start file

--- pasteMaster.py
+++ pasteMaster.py
@@ -15,9 +15,11 @@ def index():
 # def index():
 #    return render_template('start.html')
 
-# @app.route('/run.html')
+
+@app.route('/run.html')
 def run_script():
-   file = open(r'./src/pythonScript/detect.py', 'r').read()
+   # return db naam / file naam --> return foto + array
+   file = open(r'./src/pythonScript/test.py', 'r').read()
    return exec(file)
 
 @app.errorhandler(404)
