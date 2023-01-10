d14cb54bae989152d644702bb0cdebb1923c5e5b
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-10 14:43:35 +0100

    flask test

--- src/pythonScript/test.py
+++ src/pythonScript/test.py
@@ -4,7 +4,8 @@ import time
 time.sleep(10)
 
 picam2 = Picamera2()
-picam2.start_and_capture_file("test.jpg", delay=0, show_preview=False)
+picam2.start_and_capture_file("static/image/view.jpg", delay=0, show_preview=False)
+
 # path = "path/to/image.jpg"
 
 # with open(path, 'rb') as f:
