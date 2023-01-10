af04a97f59d222a59d7076e0a64e5c0a44cc2be7
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-10 14:58:23 +0100

    start test

--- pasteMaster.py
+++ pasteMaster.py
@@ -8,8 +8,9 @@ if __name__ == '__main__':
    app.run()
 
 @app.route('/')
-def home():
+def homes():
    return render_template('index.html')
+   
 @app.route('/index.html')
 def home():
    return render_template('index.html')
