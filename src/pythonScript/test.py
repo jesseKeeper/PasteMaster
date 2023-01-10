15d0ee6dc61b28607f4cf53197d13d276f96b530
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-10 14:15:49 +0100

    flask test

--- src/pythonScript/test.py
+++ src/pythonScript/test.py
@@ -1,4 +1,15 @@
+from picamera2 import Picamera2, Preview
 import time
 
 time.sleep(10)
 
+picam2 = Picamera2()
+picam2.start_and_capture_file("test.jpg", delay=0, show_preview=False)
+# path = "path/to/image.jpg"
+
+# with open(path, 'rb') as f:
+#     jpg = f.read()
+
+# Response.ContentType = "image/jpeg"
+
+# Response.WriteBinary(jpg)
