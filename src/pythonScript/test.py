db5d985528a7049acb0a39b0962f1e99181179a6
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-11 13:51:25 +0100

    start file

--- src/pythonScript/test.py
+++ src/pythonScript/test.py
@@ -6,6 +6,8 @@ time.sleep(3)
 picam2 = Picamera2()
 picam2.start_and_capture_file("static/image/view.jpg", delay=0, show_preview=False)
 
+time.sleep(3)
+
 # path = "path/to/image.jpg"
 
 # with open(path, 'rb') as f:
