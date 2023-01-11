a2e5bb0367665d4e43c447f516d3a958e642d22b
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-11 14:03:38 +0100

    start file

--- static/site.js
+++ static/site.js
@@ -56,10 +56,3 @@ jQuery (function() {
     }
 
 });
-
-setInterval(function () {
-    var request = new XMLHttpRequest();
-    request.open("GET", "data", true);
-    request.setRequestHeader("Cache-Control", "no-cache");
-    request.send();
-}, 1000);
