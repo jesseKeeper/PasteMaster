330e39e0c3db3944674a967e7b84169dfd9010d9
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-12 12:19:03 +0100

    start file

--- src/pythonScript/photo.py
+++ src/pythonScript/photo.py
@@ -2,7 +2,9 @@ from picamera import PiCamera
 from time import sleep
 
 camera = PiCamera()
-camera.rotation = 180
+# camera.rotation = 180
 camera.capture('static/image/view.jpg')
 
 sleep(5)
+
+SystemExit
