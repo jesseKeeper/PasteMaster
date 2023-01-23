ae38e8f5b6223b72f5a01fe3c36a84bafec45cf5
jesseKeeper <jesse.b.doelman@gmail.com>
2023-01-23 12:39:29 +0100

    camera

--- pasteMaster.py
+++ pasteMaster.py
@@ -7,7 +7,7 @@ import time
 from flask import Flask, render_template, send_file, request
 app = Flask(__name__)
 
-printer1 = printer.Printer("/dev/ttyUSB0", 115200, 55, 50, False)
+printer1 = printer.Printer("/dev/ttyUSB0", 115200, 55, 50)
 
 demoPadRange = [[2, 0, 0], [55, 255, 255]]
 demoPCBRange = [[135, 100, 78], [160, 255, 255]]
