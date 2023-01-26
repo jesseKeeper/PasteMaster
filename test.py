313f02c84535baae6a431a5a595d51a34ad653b1
dinges2 <0993362@hr.nl>
2023-01-26 15:05:32 +0100

    system test

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 import printer
 import time
+import sys
 
 def main():
-    printer1 = printer.Printer("/dev/ttyUSB0", 115200, 75, 57)
-    # printer1.dispense_at_points([[150,150],[150,148.5],[150,147],[150,145.5],[150,144],[150,142.5],[150,141],[150,139.5],[150,138]])
-    printer1.move_printer(150, 150, 57)
-    time.sleep(5)
+    # printer1 = printer.Printer("/dev/ttyUSB0", 115200, 75, 57)
+    # # printer1.dispense_at_points([[150,150],[150,148.5],[150,147],[150,145.5],[150,144],[150,142.5],[150,141],[150,139.5],[150,138]])
+    # printer1.move_printer(150, 150, 57)
+    # time.sleep(5)
+    print(sys.version)
+    print(sys.path)
