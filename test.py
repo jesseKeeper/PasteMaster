3abcdd29a8424e4393dde01c81b9220ee3863811
dinges2 <0993362@hr.nl>
2023-01-26 14:35:57 +0100

    Get

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import printer
 import paste
 
-paste.dispense(500)
+def main():
+    printer1 = printer.Printer("/dev/ttyUSB0", 115200, 75, 57)
+    printer1.dispense_at_points([[150,150],[150,148.5],[150,147],[150,145.5],[150,144],[150,142.5],[150,141],[150,139.5],[150,138]])
