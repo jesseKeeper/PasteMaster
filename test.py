2a40e18b61577527dba2243ca15d33ef7683024b
dinges2 <0993362@hr.nl>
2023-01-26 15:37:49 +0100

    Get

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ import time
 
 printer1 = printer.Printer("/dev/ttyUSB0", 115200, 75, 57)
 # printer1.dispense_at_points([[150,150],[150,148.5],[150,147],[150,145.5],[150,144],[150,142.5],[150,141],[150,139.5],[150,138]])
-printer1.move_printer(150, 150, 57)
+printer1.move_printer(150, 150, 57, 10000)
 time.sleep(5)
