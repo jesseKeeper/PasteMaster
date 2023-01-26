0123174b467d47b11358e128b0b6aeefb9c39f04
dinges2 <0993362@hr.nl>
2023-01-26 15:45:54 +0100

    Home

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import printer
 import time
 
 printer1 = printer.Printer("/dev/ttyUSB0", 115200, 75, 57)
+printer1.send_command("G28")
 # printer1.dispense_at_points([[150,150],[150,148.5],[150,147],[150,145.5],[150,144],[150,142.5],[150,141],[150,139.5],[150,138]])
 time.sleep(5)
 printer1.move_printer(150, 150, 57, 10000)
